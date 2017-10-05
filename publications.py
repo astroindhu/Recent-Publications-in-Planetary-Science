3c54acbf6c0955b8d8ba0b1c237876d0f3a5162a
Indhu Varatharajan <indhu.varatharajan@gmail.com>
2017-10-05 09:26:31 +0200

    Update publications.py

--- publications.py
+++ publications.py
@@ -9,14 +9,15 @@ Created by Indhu Varatharajan
 
 import webbrowser
 
-url = {jgr: "http://agupubs.onlinelibrary.wiley.com/hub/jgr/journal/10.1002/(ISSN)2169-9100/", 
-icarus: "http://www.sciencedirect.com/science/journal/aip/00191035?sdc=1",
-grl: "http://agupubs.onlinelibrary.wiley.com/hub/journal/10.1002/(ISSN)1944-8007/",
-natgeo: "http://www.nature.com/ngeo/index.html", 
-pss: "http://www.sciencedirect.com/science/journal/aip/00320633?sdc=1", 
-nature: "http://www.nature.com/nature/current_issue.html", 
-science: "http://science.sciencemag.org/content/early/recent", 
-geology: "https://pubs.geoscienceworld.org/geology/early-publication"
+url = {"jgr": "http://agupubs.onlinelibrary.wiley.com/hub/jgr/journal/10.1002/(ISSN)2169-9100/", 
+"icarus": "http://www.sciencedirect.com/science/journal/aip/00191035?sdc=1",
+"grl": "http://agupubs.onlinelibrary.wiley.com/hub/journal/10.1002/(ISSN)1944-8007/",
+"natgeo": "http://www.nature.com/ngeo/index.html", 
+"pss": "http://www.sciencedirect.com/science/journal/aip/00320633?sdc=1", 
+"jmlr": "http://www.jmlr.org/papers/",
+"nature": "http://www.nature.com/nature/current_issue.html", 
+"science": "http://science.sciencemag.org/content/early/recent", 
+"geology": "https://pubs.geoscienceworld.org/geology/early-publication"
 }
 
 for key in url.keys():
